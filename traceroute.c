299b09dc80a8742218b2da1002a956b6c9d1b114
Vaibhav Bajpai <contact@vaibhavbajpai.com>
2011-10-13 10:02:53 +0200

    recvfrom(...) done; need to make it non-blocking

--- traceroute.c
+++ traceroute.c
@@ -33,13 +33,16 @@ int traceroute(char* dest_hostname){
   struct addrinfo*        dest_addrinfo_collection;
   struct addrinfo*        dest_addrinfo_item;
   struct sockaddr*        dest_addr;
+  struct sockaddr_in nexthop_addr_in;
   int                     recv_socket;
   int                     send_socket;
-  char*                   buf = "";
+  char                    buf[512];
   char                    dest_addr_str[INET_ADDRSTRLEN];
-  int                     error;
+  char                    nexthop_addr_str[INET_ADDRSTRLEN];
+  unsigned int            nexthop_addr_in_len;
   unsigned short          iter;
   unsigned short          ttl = 1;
+  int                     error;
 
   /* resolve the domain name into a list of addresses */
   error = getaddrinfo(dest_hostname, NULL, NULL, &dest_addrinfo_collection);
@@ -62,14 +65,13 @@ int traceroute(char* dest_hostname){
       break;
     }
   }
-  freeaddrinfo(dest_addrinfo_collection);
-  
+
   /* loop until you reach the reach the destination;
      or your TTL exceeds MAX HOPS */
   
   while(1){
-    int recv_socket = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    //int recv_socket = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
+    //int recv_socket = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    int recv_socket = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
     if(recv_socket == -1){
       fprintf(stderr, "\ncannot create receive socket");
       return EXIT_FAILURE;
@@ -93,18 +95,41 @@ int traceroute(char* dest_hostname){
        perror("\nerror sending data to destination");
        return EXIT_FAILURE;
      }
+    
+    nexthop_addr_in_len = sizeof(nexthop_addr_in);
+    recvfrom(recv_socket, buf, sizeof(buf),
+             0, (struct sockaddr *)&nexthop_addr_in, &nexthop_addr_in_len);
+    get_ip_str((struct sockaddr *)&nexthop_addr_in,
+                nexthop_addr_str, INET_ADDRSTRLEN);
 
-    //recvfrom()
-    break;
+    char nexthop_hostname[NI_MAXHOST];
+    error = getnameinfo((struct sockaddr *)&nexthop_addr_in, 
+                        nexthop_addr_in_len, 
+                        nexthop_hostname, sizeof(nexthop_hostname),
+                        NULL, 0, NI_NAMEREQD);
+    if (error != 0){
+      error = getnameinfo((struct sockaddr *)&nexthop_addr_in, 
+                          nexthop_addr_in_len, 
+                          nexthop_hostname, sizeof(nexthop_hostname),
+                          NULL, 0, NI_NUMERICHOST);
+      if (error != 0){
+        fprintf(stderr, "error in getnameinfo: %s\n", gai_strerror(error));
+        return EXIT_FAILURE;
+      }
+    }                           
+    printf("\n%s, (%s)", nexthop_addr_str, nexthop_hostname);
+    ttl++;
+    close(recv_socket);
+    close(send_socket);
+    if (!strcmp(dest_addr_str, nexthop_addr_str))
+      break;
   }
 
-  close(recv_socket);
-  close(send_socket);
-  
+  freeaddrinfo(dest_addrinfo_collection);
   return EXIT_SUCCESS;
 }
 
 int main(void){
-  int status = traceroute("www.facebook.com");
+  int status = traceroute("www.linkedin.com");
   return status;
 }
